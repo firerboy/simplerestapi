08ce20de0c53438028e2bc81218bc6cd9550d991
firerboy <161255751+firerboy@users.noreply.github.com>
2024-09-23 13:39:01 +0200

    Add files via upload

---
+++ simplerestapi/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.security.PersonDetails;
+import com.example.demo.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        adminService.doAdminStuff();
+        return "admin";
+    }
+}
